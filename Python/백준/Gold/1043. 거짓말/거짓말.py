7f70cb3bbd1de585061990e0f6ef5548622d641f
parkminsuyang <shyeon16@ewha.ac.kr>
2025-07-16 02:21:42 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Gold/1043. 거짓말/거짓말.py
@@ -0,0 +1,45 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+truth=list(map(int,input().split()))
+truth=truth[1:]
+parent=[i for i in range(n+1)]
+
+
+def find(x):
+    if parent[x]!=x:
+        parent[x]=find(parent[x])
+    return parent[x]
+
+def union(a,b):
+    parent_a=find(a)
+    parent_b=find(b)
+    if parent_a!=parent_b:
+        parent[parent_b]=parent_a
+
+parties=[]
+
+for i in range(len(truth) - 1):
+    union(truth[i], truth[i+1])
+
+for _ in range(m):
+    p=list(map(int,input().split()))
+    party=p[1:]
+    parties.append(party)
+
+    for i in range(1,len(party)):
+        union(party[0],party[i])
+
+res=0
+for party in parties:
+    isTrue=False
+    for person in party:
+        for t in truth:
+            if find(t)==find(person):
+                isTrue=True
+                break
+    if isTrue==False:
+        res+=1
+print(res)
+
